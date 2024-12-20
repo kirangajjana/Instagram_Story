c6afdc19dfeefb475edc6ab5810292c921493854
kirangajjana <kiran.gajjana@gmail.com>
2024-12-20 23:42:05 +0530

    updated button

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let a=document.querySelector('.storydiv')
+let a=document.querySelector('#img1')
 let b=document.querySelector('#maindiv')
-console.log(b)
-a.addEventListener('mouseover',function()
-{
-    b.style.backgroundColor='red';
-    setTimeout(function()
-{
-    b.style.backgroundColor='white';
-},2000);
-    
+console.log(a)
+a.addEventListener('mouseover', function() {
+    b.style.backgroundImage = 'url("images/story2.jpg")'; // Set background image
+    b.style.backgroundSize = 'cover'; // Ensure the image covers the element fully
+    b.style.backgroundPosition = 'center'; // Center the image
+    b.style.backgroundRepeat = 'no-repeat'; 
+
+    setTimeout(function() {
+        console.log('time triggered')
+        b.style.backgroundImage = '';
+        b.style.backgroundColor='white'; // Change background color after 2 seconds
+    }, 2000);
 });
